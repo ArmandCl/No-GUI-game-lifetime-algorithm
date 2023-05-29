0bba977fefaf467c9d1081b4bce08ce82ccf4f32
RayanBT <btrayan21@gmail.com>
2023-05-29 18:40:08 +0200

    Conception des méthodes de la class SolutionEfficace.java

--- SAE_202/src/modele/SolutionEfficace.java
+++ SAE_202/src/modele/SolutionEfficace.java
@@ -1,5 +1,30 @@
 package modele;
 
 public class SolutionEfficace {
+    /**
+     * methode constructeur de la classe Solution et permet d'instancier les deux champs
+     * @param scenario Scenario
+     * @param joueur Joueur
+     */
+    public Solution(Scenario scenario, Joueur joueur) {
 
+    }
+
+    /**
+     * Méthode pour trouver la quête la plus proche en termes de position
+     * @param quetesDisponibles ArrayList<Quete> La liste de quete qui sont disponible ( on a eu cette liste grace à la methode getQuetesDisponibles)
+     * @return quetePlusProche Quete La quete la plus proche
+     */
+    private Quete trouverQuetePlusProche(ArrayList<Quete> quetesDisponibles) {
+
+    }
+
+
+
+    /**
+     * methode pour utiliser la solution gloutonne
+     */
+    public void solutionGloutonne() {
+
+    }
 }
