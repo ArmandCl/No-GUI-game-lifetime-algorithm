f07e395b6db79e42383475d98926d2d0ec489752
RayanBT <btrayan21@gmail.com>
2023-05-29 18:55:17 +0200

    Conception des méthodes de la class Scenario.java

--- SAE_202/src/modele/Scenario.java
+++ SAE_202/src/modele/Scenario.java
@@ -4,4 +4,44 @@ package modele;
 
 public class Scenario {
 
+    /**
+     * methode constructeur de la classe Scenario qui sert à instancier les champs
+     */
+    public Scenario() {
+
+    }
+
+    /**
+     * ajoute une quete à une liste de quetes
+     *
+     * @param quete Quete La quete qu'on veut ajouter à la liste
+     */
+    public void ajout(Quete quete) {
+
+    }
+
+    public void ajouterQueteAuScenario(int numeroScenario, Quete quete) {
+
+    }
+
+    // Autres méthodes de la classe Scenario...
+
+    /**
+     * recupere le champ chQuetes
+     *
+     * @return chQuetes ArrayList<Quete>
+     */
+    public ArrayList<Quete> getChQuetes() {
+
+    }
+
+    /**
+     * récupère le champ chTreeMapScenario
+     * @return chTreeMapScenario TreeMap<Integer, ArrayList<Quete>> L'arbre avec pour clé un numéro de scénario et pour valeur une liste de quete
+     */
+    public TreeMap<Integer, ArrayList<Quete>> getchTreeMapScenario() {
+
+    }
+}
+
 }
